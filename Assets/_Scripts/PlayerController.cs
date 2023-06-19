01346d6787dd583b545a0e4d0f5cabfc441b091b
Jakub Starzec <jakubstarzec97@gmail.com>
2023-06-19 22:07:50 +0200

    Rotating Player to targetLine and back

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Transform[] lanes;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] float rotationSpeed = 180f; 
+    [SerializeField] private Transform[] lanes;
     private int currentLane = 1;
     private int targetLane = 1;
     private bool isChangingLane = false;
-    private bool isMoving = false;
+    private bool isRotating = false;
     private Vector3 startPosition;
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
 
  private void Update()
     {
@@ -29,26 +32,40 @@ public class PlayerController : MonoBehaviour
 
                 if (swipeDirection.x < 0f)
                 {
-                    // Przesunięcie w lewo
+                    // Left Swipe
                     targetLane = Mathf.Clamp(currentLane - 1, 0, lanes.Length - 1);
+                    targetRotation = Quaternion.Euler(0f, 0f, 45f);
                 }
                 else if (swipeDirection.x > 0f)
                 {
-                    // Przesunięcie w prawo
+                    // Right Swipe
                     targetLane = Mathf.Clamp(currentLane + 1, 0, lanes.Length - 1);
+                    targetRotation = Quaternion.Euler(0f, 0f, -45f);
                 }
 
                 if (targetLane != currentLane)
                 {
-                    StartCoroutine(MoveToLane(targetLane));
+                    StartCoroutine(MoveAndRotateToLane(targetLane));
                 }
+            }
+        }
+
+        if (isRotating)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if (Quaternion.Angle(transform.rotation, targetRotation) < 1f)
+            {
+                isRotating = false;
+                transform.rotation = targetRotation;
             }
         }
     }
 
-    private IEnumerator MoveToLane(int targetLane)
+    private IEnumerator MoveAndRotateToLane(int targetLane)
     {
         isChangingLane = true;
+        startRotation = transform.rotation;
 
         float startX = transform.position.x;
         float targetX = lanes[targetLane].position.x;
@@ -59,10 +76,25 @@ public class PlayerController : MonoBehaviour
             t += Time.deltaTime * moveSpeed;
             float newX = Mathf.Lerp(startX, targetX, t);
             transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+            if (t < 0.5f)
+            {
+                float rotationProgress = t * 2f;
+                Quaternion newRotation = Quaternion.Slerp(startRotation, targetRotation, rotationProgress);
+                transform.rotation = newRotation;
+            }
+            else
+            {
+                float rotationProgress = (t - 0.5f) * 2f;
+                Quaternion newRotation = Quaternion.Slerp(targetRotation, Quaternion.identity, rotationProgress);
+                transform.rotation = newRotation;
+            }
+
             yield return null;
         }
 
         currentLane = targetLane;
         isChangingLane = false;
+        isRotating = false;
     }
 }
