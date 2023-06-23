ca7f85447611de227f43060b82c9966da43ab28c
Jakub Starzec <jakubstarzec97@gmail.com>
2023-06-23 21:27:22 +0200

    [16] End Game pannel

--- Assets/_Scripts/EndGameManager.cs
+++ Assets/_Scripts/EndGameManager.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndGameManager : MonoBehaviour
 {
     [SerializeField] private ScoreManager scoreManager;
+    [SerializeField] private GameObject gameOverCanvas;
+    [SerializeField] private Text bestScoreText;
+    [SerializeField] private Text bestScoreValue;
     void Start()
     {
         int bestScore = PlayerPrefs.GetInt("BestScore", 0);
         int currentScore = scoreManager.score;
+        gameOverCanvas.SetActive(true);
         
         Debug.Log("Your Score is " + currentScore);
         
         if(bestScore > currentScore || bestScore == currentScore)
         {
             Debug.Log("Best score is " + bestScore);
+            bestScoreValue.text = bestScore.ToString();
         }
         else if(bestScore < currentScore)
         {
             Debug.Log("NEW BEST SCORE");
             PlayerPrefs.SetInt("BestScore", currentScore);
+            bestScoreText.text = "NEW BEST SCORE!";
+            bestScoreValue.text = currentScore.ToString();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("MainScene");
         }
     }
 }
