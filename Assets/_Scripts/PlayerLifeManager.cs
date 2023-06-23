b573c74d2bcb54fb0f08dc6ad38669e4d888f202
Jakub Starzec <jakubstarzec97@gmail.com>
2023-06-23 17:10:45 +0200

    [7] Shield powerup

--- Assets/_Scripts/PlayerLifeManager.cs
+++ Assets/_Scripts/PlayerLifeManager.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerLifeManager : MonoBehaviour
 {
     [SerializeField] private GameObject scoreTrigger;
+    [SerializeField] private GameObject PlayerShield;
+    [SerializeField] private PlayerShieldManager shieldManager;
     [SerializeField] private int lifeCounter = 3;
     private void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.gameObject.tag == "Asteroid")
         {
-            lifeCounter--;
+            if(!PlayerShield.activeSelf)
+            {
+                lifeCounter--;
+            }
 
             if(lifeCounter <= 0)
             {
@@ -18,9 +23,14 @@ public class PlayerLifeManager : MonoBehaviour
                 Destroy(this.gameObject);
             }
         }
-        else
+        else if(other.gameObject.tag == "PowerupLife")
         {
             lifeCounter++;
+        }
+        else if(other.gameObject.tag == "PowerupShield")
+        {
+            PlayerShield.SetActive(true);
+            shieldManager.shieldTimer = 6f;
         }
         
         Debug.Log("Lifes Left: " + lifeCounter);
---
+++ Assets/_Scripts/PlayerShieldManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShieldManager : MonoBehaviour
+{
+    public float shieldTimer = 0f;
+    [SerializeField] private static float shieldTimerMax = 6f;
+    [SerializeField] private GameObject shieldObject;
+    [SerializeField] private Animator shieldAnimator;
+    [SerializeField] private ScoreManager scoreManagerScore;
+    [SerializeField] private int scoreForAsteroid = 150;
+
+    void Update()
+    {
+        Debug.Log(shieldTimer);
+        if(shieldTimer >= 0f)
+        {
+            shieldTimer -= Time.deltaTime;
+            
+            if(shieldTimer <= shieldTimerMax/2)
+            {
+                ShieldAnimatorActivate();
+            }
+            else
+            {
+                shieldAnimator.enabled = false;
+            }
+        }
+        else if(shieldTimer <= 0f)
+        {
+            ShieldAnimatorDeActivate();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "PowerupShield")
+        {
+            shieldTimer = shieldTimerMax;
+            shieldObject.SetActive(true);
+        }
+        else if(other.gameObject.tag == "Asteroid")
+        {
+            scoreManagerScore.score += scoreForAsteroid;
+            Debug.Log("ASTEROID DESTROYED! Points: "+ scoreManagerScore.score);
+        }
+    }
+
+    private void ShieldAnimatorActivate()
+    {
+        shieldAnimator.enabled = true;
+    }
+
+    private void ShieldAnimatorDeActivate()
+    {
+        shieldAnimator.enabled = false;
+        shieldObject.SetActive(false);
+    }
+}
